0787ef0ebbf421fc1ea700994cab1948a09bc4cc
Hoshino08 <hj-g20008@sist.ac.jp>
2021-05-11 12:28:31 +0900

    キーボードの入力処理

--- keyboard.cpp
+++ keyboard.cpp
@@ -7,3 +7,97 @@
 KEYBOARD keyboard;
 
 //関数
+
+/// <summary>
+/// キーの入力状態を更新する
+/// </summary>
+VOID AllKeyUpdate(VOID)
+{
+	//直前のキー入力をとっておく
+	for (int i = 0; i < KEY_KIND_MAX; i++)
+	{
+		keyboard.OldAllKeyState[i] = keyboard.OldAllKeyState[i];
+	}
+
+	//すべてのキーの入力状態を得る
+	GetHitKeyStateAll(keyboard.TempKeyState);
+
+	//押されてるキーの時間を更新する
+	for (int i = 0 < KEY_KIND_MAX; i++;)
+	{
+		//キーコードを押しているとき
+		if (keyboard.TempKeyState[i] != 0)
+		{
+			keyboard.AllKeyState[i]++;		//押している時間を加算
+		}
+		else
+		{
+			keyboard.AllKeyState[i] = 0;	//押している時間を0に戻す
+		}
+	}
+
+	return;
+}
+
+/// <summary>
+/// キーを押しているか、キーコードで判断する
+/// </summary>
+/// <param name="KEY_INPUT">キーコード</param>
+/// <returns>押していたらTRUE</returns>
+BOOL KeyDown(int KEY_INPUT_)
+{
+	if (keyboard.OldAllKeyState[KEY_INPUT_] != 0)	//以前は押していた
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+/// <summary>
+/// キーを上げているか、キーコードで判断する
+/// </summary>
+/// <param name="KEY_INPUT">キーコード</param>
+/// <returns>クリックしたらTRUE</returns>
+BOOL KeyDown(int KEY_INPUT_)
+{
+	if (
+		keyboard.OldAllKeyState[KEY_INPUT_] != 0	//以前は押していた
+		&& keyboard.AllKeyState[KEY_INPUT_] == 0	//現在は押していない
+		)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+
+/// <summary>
+/// キーコードを押し続けているか、キーコードで判断する
+/// </summary>
+/// <param name="KEY_INPUT_">キーコード</param>
+/// <param name="MiliTime">キーコードを押し続けているミリ秒</param>
+/// <returns></returns>
+BOOL KeyDownKeep(int KEY_INPUT_, int MiliTime)
+{
+	//1秒は1000ミリ秒
+	float MiliSec = 1000.0f;
+
+	//押し続けている時間は、ミリ秒数×FPS値
+	//例）1500ミリ秒押す/1000ミリ　→　1.5秒×60FPS = 90
+	int UpdateTime = (MiliTime / MiliSec) * 60;
+
+	if (keyboard.AllKeyState[KEY_INPUT_] > UpdateTime)
+	{
+		return TRUE;	//押し続けている
+	}
+	else
+	{
+		return FALSE;	//押し続けていない
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 //ヘッダーファイルの読み込み
 #include "DxLib.h"	//DxLibを使うときは必要
 
+#include "keyboard.h"
+
 //マクロ定義
 #define GAME_TITLE	"ゲームタイトル"	//ゲームタイトル
 #define GAME_WIDTH	1280				//ゲーム画面の幅(ウィドス)
@@ -20,7 +22,7 @@ int WINAPI WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
-	SetOutApplicationLogValidFlag(FALSE); 
+	SetOutApplicationLogValidFlag(FALSE);
 	ChangeWindowMode(TRUE);								//ウィンドウモードに設定
 	SetMainWindowText(GAME_TITLE);						//ウィンドウのタイトルの文字
 	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//ウィンドウの解像度を設定
@@ -31,79 +33,56 @@ int WINAPI WinMain(
 	SetWaitVSyncFlag(TRUE);
 	SetAlwaysRunFlag(TRUE);
 
-	//ＤＸライブラリ初期化処理
+	//ＤＸライブラリ初期化処理S
 	if (DxLib_Init() == -1)
 	{
 		// エラーが起きたら直ちに終了
 		return -1;
 	}
 
-	//資格の位置を決める
+	//ダブルバッファリングの有効化
+	SetDownScreen(DX_SOREEN_BACK);
+
+	//円の中心点
 	int X = GAME_WIDTH / 2;
 	int Y = GAME_HEIGHT / 2;
-
-	//四角の大きさを決める
-	int width = 32;
-	int height = 32;
-
-	//円の半径を決める
-	int radius = 100;
-
-	//速度
-	int Speed = 5;
-	int XSpeed = Speed;
-	int YSpeed = Speed;
-
-	SetDrawScreen(DX_SCREEN_BACK);
+	//円の半径
+	int radius = 50;
 
 	//無限ループ
-		while (1)
+	while (1)
+	{
+		if (ProcessMessage() != 0) { break; }	//メッセージを受け取り続ける
+		if (ClearDrawScreen() != 0) { break; }	//画面を消去する
+
+		//キーボード入力の更新
+		AllKeyUpdate();
+
+		//キー入力
+		if (keyDown(KEY_INPUT_UP) == TRUE)
 		{
-			//何かしらキーが押されたとき
-			if (CheckHitKeyAll() != 0)	//0のときに、何かキーが押された
-			{
-				break;	//無限ループを抜ける
-			}
+			Y--;
+		}
 
-			//メッセージを受け取り続ける
-			if (ProcessMessage() != 0)	//-1のとき、エラーやウィンドウが閉じられた
-			{
-				break;	//無限ループを抜ける
-			}
+		if (KeyDown(KEY_INPUT_DOWN) == TRUE)
+		{
+			Y++;
+		}
+		if (keyDown(KEY_INPUT_LEFT) == TRUE)
+		{
+			X--;
+		}
 
-			//画面を消去する
-			if (ClearDrawScreen() != 0) { break; }
+		if (KeyDown(KEY_INPUT_RIGHT) == TRUE)
+		{
+			X++;
+		}
 
-			//四角を描画
-			//DrawBox(
-			//	X, Y, X + width,Y +  height,
-			//	GetColor(255, 0, 0),
-			//	TRUE
-			//);
+		DrawCircle(X, Y, radius, GetColor(255, 255, 0), TRUE);
 
-			//円を描画
-			DrawCircle(
-				X, Y, radius,
-				GetColor(0, 255, 0),
-				FALSE, 5
-			);
-
-			X += XSpeed;
-
-			Y += YSpeed;
-
-			if (X < 0 || X + width > GAME_WIDTH)
-			{
-				XSpeed = -XSpeed;
-			}
-
-			if (Y < 0 || Y + height > GAME_HEIGHT)
-			{
-				YSpeed = -YSpeed;
-			}
-
-			ScreenFlip();
+		ScreenFlip();	//ダブルバッファリングした画像を描画
 	}
+
 
 
 	//ＤＸライブラリ使用の終了処理
